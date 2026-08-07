5080e392526edee9f7c5c95158819f3eff7c6f89
Mukesh Sharma <imukeshsharma04@gmail.com>
2026-08-07 14:25:39 +0530

    added navigation bar for small devices

--- app.js
+++ app.js
@@ -5,3 +5,24 @@ AOS.init({
     easing: "ease-out-cubic",
     mirror: true
 });
+
+const menuBtn = document.querySelector(".nav3");
+const navMenu = document.querySelector(".navlists");
+const navLinks = document.querySelectorAll(".navlists a");
+
+menuBtn.addEventListener("click", () => {
+
+    navMenu.classList.toggle("active");
+    console.log("Menu button clicked");
+
+});
+
+navLinks.forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navMenu.classList.remove("active");
+
+    });
+
+});
